a0eae8fd18b1dfa88f0ec70c36e6380fc6838636
SaraBolouriB <boloorisara@gmail.com>
2021-01-04 14:00:43 +0330

    updated code

--- source/main.py
+++ source/main.py
@@ -1,37 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-import pandas as pd
+from search import search_in_excel, search_in_google_scholar
 
 def get_search_keyword():
     key = input('Enter Your Keyword: ')
     return key
 
-def search_in_excel(keyword):
-    excel_file = './excel/test.xlsx'
-    articles = pd.read_excel(excel_file)
-    found_article = articles['Full Journal Title'].where(articles['JCR Abbreviated Title'] == keyword).dropna()
-    return found_article
-
-def search_in_google_scholar(search_keyword):
-    driver = webdriver.Chrome(executable_path="./chrome/chromedriver")
-    driver.get('https://scholar.google.com')
-    inputElems = driver.find_elements_by_css_selector('input[name=q]')
-    for inputElem in inputElems:
-        inputElem.send_keys(search_keyword)
-        inputElem.send_keys(Keys.ENTER)
-    articles = get_articles()
-
-    time.sleep(5)
-    driver.close()
-
-def get_articles(driver):
-    items = driver.find_elements_by_class_name('gs_rt')
-    hrefs = []
-    for item in items:
-        hrefs.append(item.find_element_by_tag_name('a').get_attribute('href'))
-    return hrefs
-    
 if __name__ == '__main__':
     keyword = get_search_keyword()
     found_article = search_in_excel(keyword)
