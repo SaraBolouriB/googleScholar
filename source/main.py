d745151b54a3793ec9b8a93a38f2ee336d56a030
mojtabaalinejad1376 <mojtaba.alinejad1376@gmail.com>
2021-01-10 20:20:26 +0330

    extract article url

--- source/main.py
+++ source/main.py
@@ -6,12 +6,12 @@ def get_search_keyword():
     return key
 
 if __name__ == '__main__':
-    main_years, main_citations, main_titles, main_summaries, main_authors, scholars = [], [], [], [], [], []
+    main_years, main_citations, main_titles, main_summaries, main_authors, main_urls, scholars = [], [], [], [], [], [], []
     articles_len = 0
     keyword = get_search_keyword()
     found_article = search_in_excel(keyword)
     print(found_article.iloc[0])
-    main_titles, main_authors, main_years, main_citations, main_summaries, articles_len = search_in_google_scholar(found_article.iloc[0])
+    main_titles, main_authors, main_years, main_citations, main_summaries, main_urls, articles_len = search_in_google_scholar(found_article.iloc[0])
     
     for i in range(articles_len):
         scholars.append({
@@ -19,9 +19,10 @@ if __name__ == '__main__':
             "author": main_authors[i],
             "year": main_years[i],
             "citation": main_citations[i],
-            "summary": main_summaries[i]
+            "summary": main_summaries[i],
+            "url": main_urls[i]
         })
 
     bibTex(scholars)
     for scholar in scholars:
-        print(f"title: {scholar['title']}\nauthor: {scholar['author']}\nyear: {scholar['year']}\ncitation: {scholar['citation']}\nsummary: {scholar['summary']}\n************************")
+        print(f"title: {scholar['title']}\nauthor: {scholar['author']}\nyear: {scholar['year']}\ncitation: {scholar['citation']}\nsummary: {scholar['summary']}\nurl: {scholar['url']}\n************************")
--- source/search.py
+++ source/search.py
@@ -23,7 +23,7 @@ def find_citation(information):
     return cited
 
 def search_in_google_scholar(search_keyword):
-    main_years, main_citations, main_titles, main_summaries, main_authors = [], [], [], [], []
+    main_years, main_citations, main_titles, main_summaries, main_authors, main_urls = [], [], [], [], [], []
 
     driver = webdriver.Chrome(executable_path="./chrome/chromedriver")
     driver.get('https://scholar.google.com')
@@ -39,6 +39,8 @@ def search_in_google_scholar(search_keyword):
     informations = driver.find_elements_by_css_selector('div.gs_a')
     cites = driver.find_elements_by_css_selector('div.gs_fl a:nth-of-type(3)')
     summaries = driver.find_elements_by_css_selector('div.gs_rs')
+    elems = driver.find_elements_by_css_selector('.gs_rt [href]')
+    urls = [elem.get_attribute('href') for elem in elems]
     articles_len = len(titles)
 
     for i in range(articles_len):
@@ -47,6 +49,7 @@ def search_in_google_scholar(search_keyword):
         main_citations.append(find_citation(cites[i].text))
         main_years.append(find_year(informations[i].text))
         main_authors.append(find_author(informations[i].text))
+        main_urls.append(urls[i])
 
     driver.close()
-    return main_titles, main_authors, main_years, main_citations, main_summaries, articles_len
+    return main_titles, main_authors, main_years, main_citations, main_summaries, main_urls, articles_len
